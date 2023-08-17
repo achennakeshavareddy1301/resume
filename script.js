37c8e26ca374e2fa03fb83d06d1f754fcd43bb22
achennakeshavareddy1301 <125818932+achennakeshavareddy1301@users.noreply.github.com>
2023-08-17 12:53:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
         sidebar.classList.toggle("show");
     });
 });
+const profileCards = document.querySelectorAll('.profile-card');
+
+function handleIntersection(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}
+
+const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+
+profileCards.forEach(profileCard => {
+  observer.observe(profileCard);
+});

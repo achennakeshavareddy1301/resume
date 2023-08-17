5f7bdb09b2d1ffb63827e8a9a379c0941bf03a15
achennakeshavareddy1301 <125818932+achennakeshavareddy1301@users.noreply.github.com>
2023-08-17 13:41:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function animateLogo() {
 
 // Event listener to animate the navbar logo on click
 document.querySelector('nav img').addEventListener('click', animateLogo);
+function showCardsOnLoad() {
+    profileCards.forEach(profileCard => {
+        profileCard.classList.add('show');
+    });
+}
+
+// Event listener to show the profile cards when the page is loaded
+window.addEventListener('load', showCardsOnLoad);
+
+// Function to animate the navbar logo on click
+function animateLogo() {
+    const logo = document.querySelector('nav img');
+    logo.classList.add('animate');
+    setTimeout(() => {
+        logo.classList.remove('animate');
+    }, 1000);
+}
+
+// Event listener to animate the navbar logo on click
+document.querySelector('nav img').addEventListener('click', animateLogo);
+

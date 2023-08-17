ad7caa9760d8fe2d197532cc675f39f7e97eed4e
achennakeshavareddy1301 <125818932+achennakeshavareddy1301@users.noreply.github.com>
2023-08-17 13:33:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-const profileCards = document.querySelectorAll('.profile-card');
-
-// Function to check if an element is in view
-function isInView(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-// Function to show the profile cards when they are in view
-function showCards() {
+function showCardsOnLoad() {
     profileCards.forEach(profileCard => {
-        if (isInView(profileCard)) {
-            profileCard.classList.add('show');
-        }
+        profileCard.classList.add('show');
     });
 }
 
-// Event listener to show the profile cards when the page is scrolled
-window.addEventListener('scroll', showCards);
+// Event listener to show the profile cards when the page is loaded
+window.addEventListener('load', showCardsOnLoad);
 
 // Function to animate the navbar logo on click
 function animateLogo() {
